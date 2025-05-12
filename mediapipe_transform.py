1d9aa8201049aacc2a33c02febdbe83ec2dbe89a
clayton-ellis <166958718+clayton-ellis@users.noreply.github.com>
2025-05-12 16:36:59 -0700

    Add files via upload

--- mediapipe_transform.py
+++ mediapipe_transform.py
@@ -8,15 +8,17 @@ from torch_geometric.data import Data
 # label: correct label for the image, 0 : A, ..., 25 : Z
 # detection_confidence: confidence threshold for hand detection
 # Returns a PyTorch Geometric Data object containing the hand landmarks and edges
-def image_to_graph(file: str, label: int, detection_confidence=0.3) -> Data:
+def image_to_graph(file: str, label: int, hands, detection_confidence=0.3) -> Data:
     # read image
     image = cv2.imread(file)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # Process with MediaPipe Hands
     mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=detection_confidence)
+    #hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=detection_confidence)
     results = hands.process(image_rgb)
+
+    if not results.multi_hand_landmarks: return -1
 
     # Create a dictionary for general hand connections
     hand_map = defaultdict(list)
@@ -27,17 +29,27 @@ def image_to_graph(file: str, label: int, detection_confidence=0.3) -> Data:
     # Create Data object
     x_list = []
     edge_list = []
+    for connection in mp_hands.HAND_CONNECTIONS:
+        src, dst = connection
+        edge_list.append([src, dst])
+        edge_list.append([dst, src]) 
     for hand_landmarks in results.multi_hand_landmarks:
         for i, landmark in enumerate(hand_landmarks.landmark):
             x_list.append([landmark.x, 1 - landmark.y, landmark.z])
-            for connection in hand_map[i]:
-                edge_list.append((i, connection))
+            #for connection in hand_map[i]:
+            #    edge_list.append((i, connection))
     
-    edge_list += [(j, i) for i, j in edge_list]  # Add reverse edges
-    edge_list = list(set(edge_list))  # Remove duplicates
+    #edge_list += [(j, i) for i, j in edge_list]  # Add reverse edges
+    #edge_list = list(set(edge_list))  # Remove duplicates
     x = torch.tensor(x_list, dtype=torch.float)
     edge_index = torch.tensor(edge_list, dtype=torch.long).t().contiguous()
-    y = torch.tensor([label], dtype=torch.float)
+    y = torch.tensor(label, dtype=torch.long)
 
-    hands.close()
+    if edge_index.size(1) > 0:
+        max_idx = edge_index.max().item()
+        if max_idx >= len(x_list):
+            print(f"Warning: Invalid edge index {max_idx} for {len(x_list)} nodes")
+            return -1
+
+    #hands.close()
     return Data(x=x, edge_index=edge_index, y=y)
